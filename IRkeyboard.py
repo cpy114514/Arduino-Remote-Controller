f27c589a97d333fa89c678e2a5e77aaeb47c29ff
cpy114514 <pinyuanchen311@outlook.com>
2025-06-27 16:38:12 -0700

    Update IRkeyboard.py

--- IRkeyboard.py
+++ IRkeyboard.py
@@ -15,12 +15,6 @@ while True:
         # Read a line from the serial port, decode it to string, and clean up any extra characters
         line = ser.readline().decode('utf-8', errors='ignore').strip()
 
-        # If the received line is the IR code for button "2"
-        if line == "Received IR code: 0xE718FF00":
-            # Simulate pressing Alt + F4 (close current window)
-            pyautogui.hotkey('alt', 'f4')
-
-        # If the received line is the IR code for button "1"
-        if line == "Received IR code: 0xF30CFF00":
-            # Simulate pressing the letter 'a'
-            pyautogui.press('a')
+        if line == "Received IR code: xxxxxx":
+            #use your own code in xxxxxx,get it in arduino ide with the code
+            pyautogui.#hotkey() or you can use press() or make your own one
